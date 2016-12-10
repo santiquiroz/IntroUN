725e7d348b4788780b6c2f53e8854af6b6144dec
Eliana López <3lianalopez@gmail.com>
2016-12-09 21:19:30 -0500

    Nuevas escenas (básicas)

---
+++ IntroUN/Assets/2-Scripts/Intro/Intro.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+    public GameObject textos;
+    private int indiceTexto;
+	// Use this for initialization
+	void Start () {
+        indiceTexto = 0;
+        
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+    public void siguienteTexto()
+    {
+        if (indiceTexto < textos.transform.childCount - 1)
+        {
+            textos.transform.GetChild(indiceTexto).transform.gameObject.SetActive(false);
+            indiceTexto++;
+            textos.transform.GetChild(indiceTexto).transform.gameObject.SetActive(true);
+
+        }
+
+    }
+}
